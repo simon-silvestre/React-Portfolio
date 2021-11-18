769bcd7d87ef09f19fdebf0ff21f97391274a3bb
Simon <silvestresimon@icloud.com>
2021-11-18 17:45:18 +0100

    Home slide left animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Home from './Components/Home/Home';
 import SocialMedia from './Components/SocialMedia';
 import Introduction from './Components/Introduction/Introduction';
@@ -7,18 +7,38 @@ import About from './Components/About/About';
 import Workflow from './Components/Workflow/Workflow';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer';
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 function App() {
+
+  useEffect(() => {
+    if (window.matchMedia("(min-width: 851px)").matches) {
+      gsap.to(".Home", {
+        scrollTrigger: {
+            trigger: ".Introduction",
+            start: "center 600",
+            toggleActions: "restart none none reverse"
+        },
+        x: '100vw',
+      });
+    }
+  }, [])
+
   return (
     <div className="App">
       <SocialMedia />
       <Home />
       <Introduction />
-      <Projects />
-      <About />
-      <Workflow />
-      <Contact />
-      <Footer />
+      <div className="section">
+        <Projects />
+        <About />
+        <Workflow />
+        <Contact />
+        <Footer />
+      </div>
     </div>
   );
 }
